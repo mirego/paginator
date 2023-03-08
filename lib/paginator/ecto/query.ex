9fe42d3f927cb69bd6fff6c20a1529d902f1e396
Mathieu Legault <mathieu.legault.5@gmail.com>
2023-03-07 22:18:47 -0500

    Add expression based cursor (#1)

--- lib/paginator/ecto/query.ex
+++ lib/paginator/ecto/query.ex
@@ -36,9 +36,9 @@ defmodule Paginator.Ecto.Query do
     where(query, [{q, 0}], ^filters)
   end
 
-  defp build_where_expression(query, [{column, order}], values, cursor_direction) do
-    value = Map.get(values, column)
-    {q_position, q_binding} = column_position(query, column)
+  defp build_where_expression(query, [{field, order} = column], values, cursor_direction) do
+    value = column_value(column, values)
+    {q_position, q_binding} = column_position(query, field)
 
     DynamicFilterBuilder.build!(%{
       sort_order: order,
@@ -50,9 +50,9 @@ defmodule Paginator.Ecto.Query do
     })
   end
 
-  defp build_where_expression(query, [{column, order} | fields], values, cursor_direction) do
-    value = Map.get(values, column)
-    {q_position, q_binding} = column_position(query, column)
+  defp build_where_expression(query, [{field, order} = column | fields], values, cursor_direction) do
+    value = column_value(column, values)
+    {q_position, q_binding} = column_position(query, field)
 
     filters = build_where_expression(query, fields, values, cursor_direction)
 
@@ -64,6 +64,14 @@ defmodule Paginator.Ecto.Query do
       column: q_binding,
       next_filters: filters
     })
+  end
+
+  defp column_value({{field, func}, _order}, values) when is_function(func) and is_atom(field) do
+    Map.get(values, field)
+  end
+
+  defp column_value({column, _order}, values) do
+    Map.get(values, column)
   end
 
   defp maybe_where(query, %Config{
@@ -102,6 +110,10 @@ defmodule Paginator.Ecto.Query do
     |> filter_values(cursor_fields, before_values, :before)
   end
 
+  # With custom column handler
+  defp column_position(_query, {_, handler} = column) when is_function(handler),
+    do: {0, column}
+
   # Lookup position of binding in query aliases
   defp column_position(query, {binding_name, column}) do
     case Map.fetch(query.aliases, binding_name) do
@@ -119,7 +131,7 @@ defmodule Paginator.Ecto.Query do
   # Without named binding we assume position of binding is 0
   defp column_position(_query, column), do: {0, column}
 
-  #  In order to return the correct pagination cursors, we need to fetch one more
+  #  In order to return the correct pagination cursors, we need to fetch one more
   # # record than we actually want to return.
   defp query_limit(%Config{limit: limit}) do
     limit + 1
